a39a17348233dcdacc79e3b6738dca0405e67a6a
Artem <qwites@gmail.com>
2022-05-13 11:01:03 +0300

    Initial Commit

---
+++ store-platform-mvp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  store-platform-mvp
+//
+//  Created by Artem Lashmanov on 5/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
